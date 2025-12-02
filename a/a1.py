d2bf548b858f543b5e65b38b8de157abf9295770
maiqlan <mai.lauren2@gmail.com>
2025-12-01 18:00:13 -0600

    more sucinct and clean solution

--- a/a1.py
+++ a/a1.py
@@ -1,25 +1,14 @@
 import sys
-import re
-
-n = sys.stdin.readline().replace('\n','')
-pattern = r"(L|R)(\d+)"
 
 def calc(r,s):
-    match = re.findall(pattern, s)
-    for a,b in match:
-        if a == "L":
-            r=(r-int(b)) % 100
-        else:
-            r=(r+int(b)) % 100
-    return r
+    a,b=s[0],int(s[1:])
+    return (r-b) % 100 if a=="L" else (r+b) % 100
 
-result=50
-pwd=0
+result,pwd=50,0
+n = sys.stdin.readline().replace('\n','')
 while n!="":
     result=calc(result,n)
     if result==0: 
         pwd+=1
-
     n=sys.stdin.readline().replace('\n','')
 print(pwd)
-
--- a/a2.py
+++ a/a2.py
@@ -1,34 +1,23 @@
 import sys
-import re
-
-n = sys.stdin.readline().replace('\n','')
-pattern = r"(L|R)(\d+)"
 
 def calc(r,s):
-    match = re.findall(pattern, s)
-    t=-1
-    for a,b in match:
-        if a == "L":
-            t=(r-int(b)) % 100
-        else:
-            t=(r+int(b)) % 100
+    a,b,t=s[0],int(s[1:]),-1
+    t=(r-b) % 100 if a=="L" else (r+b) % 100
     
-    if match[0][0]=="L" and r<=int(b) and r==0: #will be negative
-        return t, (int(b)-r) // 100
-    elif match[0][0]=="L" and r<=int(b):
-        return t, (int(b)-r) // 100 + 1
-    elif match[0][0]=="R" and (r+int(b))>=100:
-        return t, (r+int(b))//100
+    if a=="L" and r<=b and r==0: #will be negative
+        return t, (b-r) // 100
+    elif a=="L" and r<=b:
+        return t, (b-r) // 100 + 1
+    elif a=="R" and (r+b)>=100:
+        return t, (r+b)//100
         
     return t,1 if t==0 else 0
 
-result=50
-pwd=0
+result,pwd=50,0
+n = sys.stdin.readline().replace('\n','')
 while n!="":
-    r,p=calc(result,n)
+    result,p=calc(result,n)
     pwd+=p
-    result=r
-    print(r,p)
     n=sys.stdin.readline().replace('\n','')
 print(pwd)
 
